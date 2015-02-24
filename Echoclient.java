9c1aa8a4c80c6eeb91d038707b6775ad10ba98ff
cvoinf <schieder@cvo-bonn.de>
2015-02-24 08:54:23 +0100

    TicTacTor09Feb2015

---
+++ Echoclient.java
@@ -0,0 +1,110 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Echoclient extends Client
+{
+    // Bezugsobjekte
+    private GUI zGUI;
+    private String zIP;
+    private int zPort;
+    private String nachricht;
+    private int  ArrayTicBox[][][];
+
+    private boolean verbunden;
+
+    private int symbol;
+    private String spielerNummer;
+    
+    private boolean zugAktiv;
+
+    // Attribute
+    // Konstruktor
+    // Konstruktor
+    public Echoclient(String pIP,int pPort, GUI pGUI)
+    {
+        super(pIP, pPort);  
+        zIP = pIP;
+        zPort = pPort;
+
+        ArrayTicBox = new int[12][4][4];
+        zGUI = pGUI;
+        zugAktiv = false;
+        zGUI.deaktiviere(); 
+    }
+
+    // Dienste
+    public void processMessage(java.lang.String text)
+    {
+        nachricht = text;
+        String b[] = text.split(":");
+        if (b[0].compareTo("symbol")==0) //Spieler1 hat Kreuz, 2 hat Kreis
+        {
+            if(b[1]== "Kreuz")
+            {
+                symbol = 1;
+                spielerNummer = "spieler1";
+            }
+            else if(b[1]== "Kreis")
+            {
+                symbol = 2;
+                spielerNummer = "spieler2";
+            }
+        }
+        else if (b[0].compareTo("zugBeginnt")==0)
+        {
+            System.out.println("Client: zugBeginnt");
+            zugAktiv = true;
+            zGUI.aktiviere();
+        }
+        else if (b[0].compareTo("nichtAmZug")==0)
+        {
+            System.out.println("Client: nicht am Zug");
+            zugAktiv = false;
+            zGUI.deaktiviere();
+        }
+        else if (b[0].compareTo("wuerfel")==0)
+        {
+            System.out.println("Client: compareToWürfel");
+            String c[] = b[1].split(",");
+            zGUI.gewuerfelt(Integer.parseInt(c[0]),Integer.parseInt(c[1]));
+        }        
+        else if (b[0].compareTo("aktualisiere")==0)
+        {
+            System.out.println("Client: compareToAktualisiereAnfang");
+            int zaehler=1;
+            for (int bigBox=1 ; bigBox<=9; bigBox++) 
+            {
+                for (int column=1; column <=3; column++)
+                {
+                    for (int row=1; row <=3; row ++)
+                    {
+
+                        ArrayTicBox[bigBox][row][column] = Integer.parseInt( b[zaehler]);
+                        zaehler++;
+
+                    }
+                }            
+            }
+            zGUI.aktualisiere(ArrayTicBox);
+            System.out.println("Client: compareToAktualisiereEnde");
+        }
+        else if (b[0].compareTo("gewonnen")==0)
+        {
+            if(b[1]== spielerNummer)
+            {
+                zGUI.gewonnen();
+            }
+            else
+            {
+                zGUI.verloren();
+            }
+        }
+    }
+    
+    public void knopfGedrueckt(int bigBox, int row, int column)
+    {
+        send("feld:"+bigBox+","+row+","+column);
+    }
+}
---
+++ GUI.java
@@ -0,0 +1,288 @@
+
+import sum.komponenten.*;
+import sum.werkzeuge.*;
+import sum.ereignis.*;
+
+/**
+ * Die Klasse SuMAnwendung wurde nicht automatisch erstellt: 
+ * 
+ * @author Die üblichen Verdächtigen
+ * @version 15.01.2014
+ */
+
+public class GUI extends EBAnwendung
+{
+    // Objekte
+    //private Etikett hatEtikett1;
+
+    // Hier werden die ticBoxen, also die einzelnen Knöpfe in einem Array definiert und die Höhe, Breite etc. festgelegt.
+    private Knopf ticBox[][][];
+    final int startx = 80;
+    final int starty = 30;
+    final int abstandBoxen = 3;
+    final int ticBoxWidth=50;
+    final int ticBoxHeight=50;
+    final int distance = 5;
+
+    // Hier wird der Reset Knopf definiert.
+    private Knopf reset;
+    final double pLinks = 130;
+    final double pOben = 100;
+    final double pBreite =100;
+    final double pHoehe = 30;
+    final String pAufschrift = "Reset";
+
+    // Hier wird der Verbinden Knopf definiert.
+    private Knopf Verbinden;
+    final double aLinks=230 ;
+    final double aOben = 100;
+    final double aBreite =100;
+    final double aHoehe = 30;
+    final String aAufschrift = "Verbinden";
+
+    // Hier wird das Textfeld für die Ip-Eingabe definiert.
+    private Textfeld textfeldAddresse;
+    final double tLinks = 130;
+    final double tOben = 50;
+    final double tBreite =500;
+    final double tHoehe = 30;
+    final String tText = "10.68.112.6";
+
+    // Hier wird das Textfeld für die Port-Eingabe definiert.
+    private Textfeld textfeldPort;
+    final double tLinks2 = 130;
+    final double tOben2 = 10;
+    final double tBreite2 =500;
+    final double tHoehe2 = 30;
+    final String tText2 = "555";  
+
+    // Hier wird das Würfel Ergebnis ausgegeben.
+    private Etikett wuerfelErgebnis;
+    final double cLinks = 130;
+    final double cOben = 130;
+    final double cBreite = 200;
+    final double cHoehe = 30;
+    final String cText = "Würfel 1: "+"Würfel 2:";
+
+    // Hier wird der Knopf zum Würfeln definiert.
+    private Knopf Würfeln;
+    final double bLinks = 330;
+    final double bOben = 100;
+    final double bBreite = 100;
+    final double bHoehe = 30;
+    final String bAufschrift = "Würfeln";
+
+    final boolean DEBUG = true;
+
+    // Es wird der Echoclient vorausgesetzt.
+    private Echoclient echo;
+
+    // Attribute
+    /**
+     * Konstruktor
+     */
+    public GUI()
+    {
+        //Initialisierung der Oberklasse
+        super(700, 750); 
+        int i=0;
+
+        /**
+         * Hier werden die einzelnen ticBoxen aufwendig erzeugt. Dazu wird erst einmal das Array definiert,
+         * es umfasst [12](-3) BigBoxen für die einzelnen Würfelergebnisse, und je [4] für Breite und Höhe.
+         * Dann werden erst die bigBoxen durchgezählt, dann die Spalten, dann die Reihen.
+         * Nach und nach wird nun mittels i hochgezählt, es werden ticBoxen erzeugt, wobei der Abstand, die Breite und die Höhe miteingerechnet wird.
+         * Zusätzlich bekommen die ticBoxen Knöpfe, welche auch den wert "knopfGeklickt" haben.
+         */
+        ticBox = new Knopf [12][4][4];
+        for(int bigBox = 1; bigBox <=9; bigBox++)
+        { for (int column = 1; column<4; column++)
+            { for (int row = 1; row < 4; row++)
+                {
+                    i++;
+                    int x=startx+((bigBox+2)%3)*3*(ticBoxWidth+abstandBoxen)+row*(ticBoxWidth);
+                    int y= starty+(((int)((bigBox+2)/3.0))*(ticBoxHeight+abstandBoxen))*3+column*(ticBoxHeight);
+
+                    ticBox[bigBox][row][column] = new Knopf(x,y, ticBoxWidth, ticBoxHeight, ""+i, "knopfGeklickt");
+                }  
+            }
+        }
+        if ( DEBUG == true) System.out.println("GUI: es wurden alle Knöpfe erzeugt.");
+        /**
+         * Es werden Knöpfe erzeugt: Reset, Verbinden, Adress- und Portfeld, sowie Würfeln und das Textfeld für die Ausgabe des Würfelergebnisses.
+         */
+        reset = new Knopf(pLinks, pOben, pBreite, pHoehe, pAufschrift);
+        //Aufgeben = new Knopf(aLinks, aOben, aBreite, aHoehe, aAufschrift);
+        Verbinden = new Knopf(aLinks, aOben, aBreite, aHoehe, aAufschrift);
+        Verbinden.setzeBearbeiterGeklickt("VerbindenGeklickt");     
+
+        textfeldAddresse = new Textfeld( tLinks, tOben, tBreite, tHoehe, tText);
+        textfeldPort = new Textfeld (tLinks2, tOben2, tBreite2, tHoehe2, tText2);
+
+        Würfeln = new Knopf(bLinks, bOben, bBreite, bHoehe, bAufschrift);
+        Würfeln.setzeBearbeiterGeklickt("WuerfelnGeklickt");
+
+        wuerfelErgebnis = new Etikett(cLinks,cOben,cBreite,cHoehe,cText);
+    }
+
+    /**
+     * Hier wurde der Knopf "Verbinden" geklickt, wodurch dem Echoclient übergeben wird, mit welcher
+     * Ip-Adresse und welchem Port man sich verbinden will.
+     */
+    public void VerbindenGeklickt()
+    {
+        echo = new Echoclient(textfeldAddresse.inhaltAlsText(), Integer.parseInt(textfeldPort.inhaltAlsText()), this);
+
+        if ( DEBUG == true) System.out.println("GUI: VerbindenGeklickt: " + "IP" +textfeldAddresse.inhaltAlsText()+ " Port "+textfeldPort.inhaltAlsText());
+    }
+
+    /**
+     * Hier können alle ticBoxen deaktiviert werden. Dies sollte nach jedem Spielzug geschehen.
+     */
+    public void WuerfelnGeklickt()
+    {
+        echo.send("wuerfeln");
+    }
+
+
+    public void deaktiviere()
+    { for (int bigBox = 1; bigBox <=9; bigBox++)
+        { for (int column = 1; column<4; column++)
+            { for (int row = 1; row < 4; row++)
+                    ticBox[bigBox][row][column].deaktiviere();
+            }
+        }
+        if ( DEBUG == true) System.out.println("GUI: deaktiviere()");
+    }
+
+    /**
+     * Hier können spezifische ticBoxen in bigBoxen angesteuert werden. Dazu werden lediglich Zeile und Spalte übergeben.
+     */
+    public void aktiviere(int row,int column)
+    {
+        for (int bigBox = 1; bigBox <= 9; bigBox++)
+        {
+            if(Integer.parseInt(ticBox[bigBox][row][column].inhaltAlsText())==0)
+            {
+                ticBox[bigBox][row][column].aktiviere();
+            }
+        }
+        if ( DEBUG == true) System.out.println("GUI: aktiviere ticBox " + row + column);
+    }
+
+    /**
+     * Hier kann das gesamte Feld aktiviert werden. Dies sollte lediglich als Dummy-Funktion bereit sein.
+     */
+    public void aktiviere()
+    {
+        for (int bigBox = 1; bigBox <=9; bigBox++)
+        { for (int column = 1; column<4; column++)
+            { 
+                for (int row = 1; row < 4; row++)
+                { if(Integer.parseInt(ticBox[bigBox][row][column].inhaltAlsText())==0)
+                    {
+                        ticBox[bigBox][row][column].aktiviere();
+
+                    }
+                }
+            }
+        }
+        if ( DEBUG == true) System.out.println("GUI: Es wurde alles aktiviert. Achtung, dies ist lediglich eine Dummy-Funktion!");
+    }
+
+    /**
+     * Hier wird eine ticBox geklickt. Dabei wird erkannt, in welcher bigBox, Reihe und Spalte diese liegt,
+     * was wiederum dem Client übergeben wird. Im Nachhinein werden alle ticBoxen deaktiviert.
+     */
+    public void knopfGeklickt()
+    {
+        int geklicktBigbox=0;
+        int geklicktRow=0;
+        int geklicktColumn=0;
+
+        for(int bigBox = 1; bigBox <= 9; bigBox++)
+        { for (int column = 1; column<4; column++)
+            { for (int row = 1; row < 4; row++)
+                {
+                    if (ticBox[bigBox] [row] [column].besitztFokus())
+                    {
+                        geklicktBigbox = bigBox;
+                        geklicktRow= row;
+                        geklicktColumn=column;
+                    }
+                }  
+            }
+        }
+
+        echo.knopfGedrueckt(geklicktBigbox, geklicktRow, geklicktColumn);
+        deaktiviere();
+        if ( DEBUG == true) System.out.println("GUI: knopfGeklickt "+" Reihe "+geklicktRow+" Spalte "+geklicktColumn);
+    }
+
+    /**
+     * Das gesamte Feld wird aktualisiert, indem wir den Inhalt der ticBoxen gleich dem Array vom Client setzen.
+     */
+    public void aktualisiere(int pFeld[][][])
+    {
+        for (int bigBox=1 ; bigBox<=9; bigBox++) 
+        {
+            for (int column=1; column <=3; column++)
+            {
+                for (int row=1; row <=3; row ++)
+                {
+                    ticBox[bigBox][row][column].setzeInhalt(pFeld[bigBox][row][column]);
+                }
+            }            
+        }      
+        if ( DEBUG == true) System.out.println("GUI: aktualisiere wurde aufgerufen");
+    }
+
+    /**
+     * Yay, wir haben das Würfelergebnis bekommen. Ab damit ins Etikett und aktiviert die ticBoxen!
+     */
+    public void gewuerfelt(int pa, int pb)
+    {
+        deaktiviere(); 
+
+        int e = pa+pb;
+        switch (e) {
+            case 3:  aktiviere(1,1);
+            break;
+            case 4: aktiviere(1,2);
+            break;
+            case 5: aktiviere(1,3);
+            break;
+            case 6:  aktiviere(2,1);
+            break;
+            case 7:  aktiviere(2,2);
+            break;
+            case 8:  aktiviere(2,3);
+            break;
+            case 9:  aktiviere(3,1);
+            break;
+            case 10:  aktiviere(3,2);
+            break;
+            case 11: aktiviere(3,3);
+            break;
+
+        }
+        if ( DEBUG == true) System.out.println("GUI: es wurde gewürfelt!");
+    }
+
+    /**
+     * Spiel, Satz und Sieg.
+     */
+    public void gewonnen()
+    {
+
+    }
+
+    /**
+     * Sei jetzt bloß kein schlechter Verlierer.
+     */
+    public void verloren()
+    {
+
+    }
+}
+
